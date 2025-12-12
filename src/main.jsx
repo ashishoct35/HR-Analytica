bbcdc37e087dc15276c839e22f14428c6844d0bc
Ashishoct35 <ashishoct35@gmail.com>
2025-12-12 14:13:04 +0545

    Fix styling issue by using separate dropdown.css file

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './dropdown.css' // Import custom dropdown styles
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
